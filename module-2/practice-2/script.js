65855157c22571fd8162e139e44c939731d7ead4
Zoja7 <yaozonka@gmail.com>
2023-07-26 12:46:40 +0300

    4th commit

--- module-2/practice-2/script.js
+++ module-2/practice-2/script.js
@@ -1,5 +1,4 @@
 "use strict";
-
 //* Function declaration
 // hoisting https://www.w3schools.com/js/js_hoisting.asp#
 
@@ -74,11 +73,13 @@
 //     }
 // }
 
-// const robotAnswer = robot(true);
+// const robotAnswer = robot(false);
 // console.log(robotAnswer)
 
 //* Example 5
-// Приклад - Impure function (брудна функція) - змінює вхідні дані в процесі виконання
+// Приклад - Impure function (брудна функція)
+//- змінює вхідні дані в процесі виконання
+
 // let someArray = ['Hello', 'my', 'name', 'scope'];
 
 // function checkScope(arr) {
@@ -89,7 +90,11 @@
 // console.log(someArray);
 
 //------
-// Приклад - Pure function (чиста функція) - не змінює вхідні дані, а на їхній основі просто видає новий результат(правильний підхід до написання функцій)
+// Приклад - Pure function (чиста функція) -
+//не змінює вхідні дані,
+//а на їхній основі просто видає новий результат
+//(правильний підхід до написання функцій)
+
 // let someArray = ['Hello', 'my', 'name', 'scope'];
 
 // function checkScope(arr) {
@@ -104,52 +109,58 @@
 // Практика
 
 //* Task-1;
-// Створи функцію яка буде перевіряти чи кожен елемент масиву більше ніж вказане значення. Функція приймає два параметри.
+// Створи функцію яка буде перевіряти
+//чи кожен елемент масиву більше ніж вказане значення.
+//Функція приймає два параметри.
 // 1 - Масив чисел
 // 2 - Число яке потрібно порівнювати з усіма елементами масиву.
 // Функція повертає повідомлення про успішну або не успішну перевірку (повідомлення "Success ✅" "Fail ❌")
 
-const numbers = [25, 12, 67, 40, 18];
+// const numbers = [25, 12, 67, 40, 18];
 
-function checkValue(numbers, value) {
+// function checkValue(numbers, value) {
   //Example 1
-  //   let message = "Success ✅";
-  //   for (const num of numbers) {
-  //     if (value < num) {
-  //       message = "Fail ❌";
-  //       return message;
-  //     }
-  //   }
-  //     return message;
+    // let message = "Success ✅";
+    // for (const num of numbers) {
+    //   if (value < num) {
+    //     message = "Fail ❌";
+    //     return message;
+    //   }
+    // }
+    //   return message;
   //Example 2
-  // const maxArrayValue = Math.max(...numbers);
-  // return maxArrayValue > value ? "Fail ❌" : "Success ✅";
-}
+//   const maxArrayValue = Math.max(...numbers);
+//   return maxArrayValue > value ? "Fail ❌" : "Success ✅";
+// }
 
 // console.log(checkValue(numbers, 100));
 // console.log(checkValue(numbers, 13));
 
 //* Task-2
-// Створити функцію яка буде розбивати початковий масив на потрібну кількість елементів розділяючи на декілька масивів.Функція приймає 2 параметри
+// Створити функцію яка буде розбивати початковий масив
+//на потрібну кількість елементів
+//розділяючи на декілька масивів.
+//Функція приймає 2 параметри
+
 // 1 - масив значень
 // 2 - потрібну кількість елементів в масиві
 //  Функція повертає масив масивів
 
 // const data = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-function getCombination(arr, count) {
-  const result = [];
+// function getCombination(arr, count) {
+//   const result = [];
 
-  for (let i = 0; i < arr.length; i += count) {
-    const shortArray = arr.slice(i, i + count);
-    //  shortArray.length === count ? result.push(shortArray) : null;
-    if (shortArray.length === count) {
-      result.push(shortArray);
-    }
-  }
+//   for (let i = 0; i < arr.length; i += count) {
+//     const shortArray = arr.slice(i, i + count);
+//     //  shortArray.length === count ? result.push(shortArray) : null;
+//     if (shortArray.length === count) {
+//       result.push(shortArray);
+//     }
+//   }
 
-  return result;
-}
+//   return result;
+// }
 // console.log(getCombination(data, 2)); // [[1, 2], [3, 4],[5, 6],[7, 8]]
 // console.log(getCombination(data, 3)); // [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
@@ -170,9 +181,9 @@ function getCombination(arr, count) {
 //* Task-4
 //? Напишіть функції для роботи з колекцією навчальних курсів courses:
 
-//? addCourse(name) - додає курс до кінця колекції
-//? removeCourse(name) - видаляє курс із колекції
-//? updateCourse(oldName, newName) - змінює ім'я на нове
+// ? addCourse(name) - додає курс до кінця колекції
+// ? removeCourse(name) - видаляє курс із колекції
+// ? updateCourse(oldName, newName) - змінює ім'я на нове
 
 // const courses = ["HTML", "CSS", "JavaScript", "React", "PostgreSQL"];
 
